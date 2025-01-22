bbf38c6e62e86c7dc47b010904b0bb5c4a57f217
Aysahnur <aiishaysahh@gmail.com>
2025-01-22 16:08:26 +0700

    Latihan1 Aysah Nur Hikmah

---
+++ Latihan1.java
@@ -0,0 +1,14 @@
+package com.belajar;
+
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        String nama = "Aysah";
+        String kelas = "X PPLG 2";
+        double B_badan = 46.00;
+        System.out.println("Nama Saya : " +nama);
+        System.err.println("Kelas : " +kelas);
+        System.err.print("Berat Badan : ");
+        System.out.println(B_badan);
+    }
+}
